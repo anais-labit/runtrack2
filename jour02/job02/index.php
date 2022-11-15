2e7aaf8d6b83c0c9cca216573e76703b92ba3437
anais-labit <anais.labit@gmail.com>
2022-11-15 12:21:06 +0100

    ajout job02

---
+++ jour02/job02/index.php
@@ -0,0 +1,23 @@
+<?php
+    for ($i = 0; $i <= 1337; $i++) {
+        switch ($i) {
+            case 26:
+                echo "<br>";
+                break;
+            case 37:
+                echo "<br>";
+                break;
+            case 88:
+                echo "<br>";
+                break;
+            case 1111:
+                echo "<br>";
+                break;
+            /* case 3233:
+                echo "<br>";
+                break; */
+            default;
+                echo "$i<br>";
+            }
+        }
+?>
