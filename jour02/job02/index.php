fd388d15a71caf9f95abdafb5c869fd97646d6f6
anais-labit <anais.labit@gmail.com>
2022-11-15 17:39:29 +0100

    ajout job5 et push de fin de journée

--- jour02/job02/index.php
+++ jour02/job02/index.php
@@ -2,6 +2,7 @@
 
 <?php
 
+// option pour remplacer les chiffres voulus par un br
     for ($i = 0; $i <= 1337; $i++) {
         switch ($i) {
             case 26 : case 37 : case 88 : case 1111 : case 3233 :
@@ -13,11 +14,11 @@
             }
         }
 
-
-    // for ($i = 0; $i <= 1337; $i++) {
-    //     if ($i == 26) { 
-    //             continue;
-    //         }
-    //         echo "$i<br>";
-    //     }
+// // option pour sauter le tour de boucle qui correspond au chiffre         
+//     for ($i = 0; $i <= 1337; $i++) {
+//         if ($i == 26) { 
+//                 continue;
+//             }
+//             echo "$i<br>";
+//         }
 ?>
