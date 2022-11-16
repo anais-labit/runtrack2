4e6b1c182855e04f6bf4b0e9f0d99980bc5336cd
anais-labit <anais.labit@gmail.com>
2022-11-16 13:54:09 +0100

    ajout job4 jour 3 - compter et afficher le nombre de caractères d'une chaîne grâce à un counter qui s'incrémente à chaque passage sur une nouvelle lettre

---
+++ jour03/job04/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$str="Dans l'espace, personne ne vous entend crier. <br/>";
+$counter=0;
+
+for ($i = 0; isset($str[$i]); $i++){
+    $counter++;
+    echo $str[$i];
+}
+
+echo "Cette phrase contient <b> $counter </b> caractères.";
