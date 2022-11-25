0bf13e7a68c5c576467e130822951cf42f4e3f65
anais-labit <anais.labit@gmail.com>
2022-11-25 15:29:58 +0100

    job 8 jour 10

---
+++ jour10/job08/index.php
@@ -0,0 +1,44 @@
+<?php
+$conn = new mysqli('localhost', 'root', '', 'jour09');
+$request = $conn->query("SELECT SUM(capacite) FROM salles");
+$result = $request->fetch_all();
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job 8 jour 10</title>
+</head>
+
+<body>
+    <table border="1px">
+        <thead>
+            <th>Capacité totale</th>
+        </thead>
+        <tbody>
+            <td>
+                <?php
+                foreach ($result as $ligne) {
+                    echo "<tr>";
+                    foreach ($ligne as $value) {
+                        echo "<td>" . $value . "</td>";
+                    }
+                    echo "</tr>";
+                }
+
+                ?>
+            </td>
+        </tbody>
+    </table>
+
+
+
+</body>
+
+</html>
