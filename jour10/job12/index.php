1f4b799df9b44eba1e2dc5f543bd2b348ba39aa0
anais-labit <anais.labit@gmail.com>
2022-11-25 16:35:21 +0100

    job 12 jour 10 - récupérer etudiants dont la date de naissance est entre 1998 et 2018

---
+++ jour10/job12/index.php
@@ -0,0 +1,48 @@
+<?php
+
+$conn = new mysqli('localhost', 'root', '', 'jour09');
+$request = $conn->query("SELECT prenom, nom, naissance FROM etudiants WHERE naissance BETWEEN '1998-01-01' AND '2018-12-31'");
+$result = $request->fetch_all();
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job 12 jour 10</title>
+</head>
+
+<body>
+
+    <table>
+        <thead>
+            <th>Prénom</th>
+            <th>Nom</th>
+            <th>Naissance</th>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($result as $ligne) {
+                echo "<tr>";
+                foreach ($ligne as $value) {
+                    echo "<td>" . $value . "</td>";
+                }
+                echo "</tr>";
+            }
+
+            ?>
+
+
+
+        </tbody>
+
+    </table>
+
+
+</body>
+
+</html>
