5706d2900040dd6ee6842dc7346a7d3fa3d0264e
anais-labit <anais.labit@gmail.com>
2022-11-25 13:58:54 +0100

    ajout job4 jour 10

---
+++ jour10/job04/index.php
@@ -0,0 +1,43 @@
+<?php
+
+$conn = new mysqli('localhost', 'root', '', 'jour09');
+$request = $conn->query("SELECT * FROM etudiants WHERE prenom LIKE 'T%'");
+$result = $request->fetch_all();
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job 4 jour 10</title>
+</head>
+
+<body>
+
+    <table>
+        <thead>
+            <th>Prénom</th>
+            <th>Nom</th>
+            <th>Naissance</th>
+            <th>Sexe</th>
+            <th>Email</th>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($result as $ligne) {
+                echo "<tr>";
+                foreach ($ligne as $value) {
+                    echo "<td>" . $value . "</td>";
+                }
+            }
+            ?>
+        </tbody>
+    </table>
+
+</body>
+
+</html>
